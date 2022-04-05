51368a3a5b34be1c3b162439fca9c0573f0cdefc
nandobfer <nandobfer@yahoo.com.br>
2022-04-05 03:39:21 -0300

    added server and client files

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket            
+ 
+# next create a socket object
+s = socket.socket()        
+ 
+# reserve a port
+port = 7171               
+ 
+# bind to the port
+s.bind(('', port))        
+print(f'socket binded to {port}')
+ 
+# put the socket into listening mode, number of max connections while server is busy 
+s.listen(5)    
+print("socket is listening")           
+ 
+# a forever loop
+while True:
+ 
+# Establish connection with client.
+  c, addr = s.accept()    
+  print('Got connection from', addr )
+ 
+  # send data to the client. encoding to send byte type.
+  c.send('galinha pintadinha'.encode())
+  
+  # print data received from client, decoded as string
+  print(c.recv(1024).decode())
+ 
+  # Close the connection with the client
+  c.close()
+   
+  # Breaking once connection closed
+#   break
