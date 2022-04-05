9f3344a1fb902eb8133a512c4cea62bed2318dc4
nandobfer <nandobfer@yahoo.com.br>
2022-04-05 03:18:17 -0300

    added methods

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 # save this as app.py
-from flask import Flask, escape, request
+from flask import Flask, escape, request, url_for, redirect
 
 app = Flask(__name__)
 
+# url
 @app.route('/')
-def hello():
-    name = request.args.get("name", "Page")
-    return f'<h1>Home, {escape(name)}!</h1>'
+def index():
+    # redirecting to another endpoint
+    return redirect(url_for('home'))
 
-@app.route('/about')
-def about():
-    # name = request.args.get("name", "Page")
-    return f'<h1>About Page!</h1>'
+@app.route('/home')
+def home():
+    name = request.args.get("name", "Page")
+    return f'<h1>Home {escape(name)}!</h1>'
+
+# url/ recebe o resto como variavel e passa pra função
+@app.route('/<name>')
+def hello(name):
+    return f'<h1>Hello, {escape(name)}!</h1>'
+
+# printa no console
+with app.test_request_context():
+    print(url_for('home'))
+    # url_for('nomeDaFunção', argumentos da função = 'args')
+    print(url_for('hello', name='John Doe'))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="7171")
